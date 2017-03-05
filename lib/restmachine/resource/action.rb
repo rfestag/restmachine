0ab7bf3633a5b7fd4e570cafc8c953168ce65ae0
Ryan Festag <rfestag@gmail.com>
2017-03-05 16:18:07 -0700

    Missing action, trying to use standard exceptions

--- lib/restmachine/resource/action.rb
+++ lib/restmachine/resource/action.rb
@@ -1,41 +1,18 @@
 require 'json'
 module Restmachine
   module Resource
-    class Item < Model
+    class Action < Model
       def allowed_methods
-        %w(OPTIONS GET PUT POST DELETE)
-      end
-      def allow_missing_put?
-        true
-      end
-      def is_conflict?
-        if request.put? and resource.nil?
-          !allow_missing_put?
-        else
-          false
-        end
+        %w(OPTIONS POST)
       end
       def forbidden?
-        if resource
-          if request.get?
-            @action = :show
-            authorize(resource, :show?)
-          elsif request.put?
-            @action = :update
-            authorize(resource, :update?)
-          elsif request.post?
-            action = request.path_info[:id]
-            check = (action+'?').to_sym
-            @action = action.to_sym
-            begin
-              authorize(model, check)
-            rescue NoMethodError => e
-              handle_unauthorized(Pundit::NotDefinedError.new("unable to find authorization for #{action}"))
-              return true
-            end
-          elsif request.delete?
-            @action = :delete
-            authorize(resource, :delete?)
+        if resource 
+          check = (action+'?').to_sym
+          begin
+            authorize(resource, check)
+          rescue NoMethodError => e
+            handle_unauthorized(Pundit::NotDefinedError.new("unable to find authorization for #{action}"))
+            return true
           end
         end
         #The 'authorize' methods don't return anything. If we get here,
@@ -54,11 +31,10 @@ module Restmachine
         #We do it this way for cases where a resource
         #doesn't exist. We don't want to look up more than once
         return @resource if @lookup_done
-        if collection_methods.map{|m| m.to_s}.include? id
-          @resource = model
-        else
-          @resource = show
-        end
+        @resource = show
+        return false unless @resource
+        @action = item_methods.map{|m| m.to_s}.include?(action) ? action.to_sym : nil
+        return false unless @action
         @lookup_done = true
         @resource
       end
@@ -66,26 +42,19 @@ module Restmachine
         attributes = validated_attributes(params, resource, @action)
         if attributes.respond_to? :messages
           if attributes.messages.length == 0
-            if request.post?
-              attributes == nil ? self.class.send(@action) : self.class.send(@action, attributes.to_h)
-              generate_post_response
-              true
-            else
-              update attributes.to_h
-            end
+            attributes == nil ? send(@action) : send(@action, attributes.to_h)
+            generate_post_response
+            true
           else
             errors << attributes.messages
             generate_post_response
             422
           end
         else
-          attributes == nil ? self.class.send(@action) : self.class.send(@action, attributes.to_h)
+          attributes == nil ? send(@action) : send(@action, attributes.to_h)
           generate_post_response
           200
         end
-      end
-      def handle_delete
-        delete
       end
       def to_json
         if errors.empty?
